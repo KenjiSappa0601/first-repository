be2c49b46a6c70c077febbfac6965e28f113a80a
KenjiSappa0601 <kisa765058@gmail.com>
2021-07-30 11:37:47 +0900

    8-5

--- blog/resources/views/show.blade.php
+++ blog/resources/views/show.blade.php
@@ -9,8 +9,8 @@
     </head>
     <body>
         <h1>Blog Name</h1>
+        <p class='edit'>[<a href="/posts/{{ $post->id }}/edit">edit</a>]</p>
         <div class='post'>
-
             <h2 class='title'>{{ $post->title }}</h2>
             <p class='body'>{{ $post->body }}</p>
             <p class='updated_at'>{{ $post->updated_at }}</p>
